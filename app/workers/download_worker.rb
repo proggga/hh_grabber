9a358a97da7746331f1ca9e5a8762063ec82017a
Mikhail Fesenko <proggga@gmail.com>
2016-09-13 16:31:45 +0300

    now it parse pages

--- app/workers/download_worker.rb
+++ app/workers/download_worker.rb
@@ -5,39 +5,51 @@ class DownloadWorker
   include Sidekiq::Worker
   sidekiq_options :queue => :download, :retry => false, :backtrace => false
 
-  def perform(keyword)
+  def perform(keyword, main_url=nil, worker_page_id=1, add_pages=true)
     user_agent = 'curl/7.24.0'
+    main_server = "https://spb.hh.ru"
     headers = { 'Content-Type' => 'text/html; charset=utf-8' }
     tg = TinyGrabber.new
-    print "START\n"
     tg.debug = { active: true, destination: :print, save_html: true }
     tg.headers = headers
     tg.user_agent = user_agent
-    main_url = "https://spb.hh.ru/search/vacancy?text=#{keyword}"
+    main_url = main_server + "/search/vacancy?text=#{keyword}" if not main_url
     url = main_url.dup
     begin
-      print "LLOPIN\n"
       response = tg.get url, headers
-      print "AFFTER GET\n"
       url = response['location']
     end until main_url == response['location'] or response.instance_of?(Net::HTTPOK)
     #
     #print response.body
     ng = response.ng
-    urls = []
+    vacancies_urls = {}
     ng.css('a.search-result-item__name').each do |alink|
-      urls.push([alink.text, alink['href']])
+      vacancies_urls[alink.text] = alink['href']
     end
-    puts "yolo", urls
-    return response.body
-    ng = response.ng
-    new_url = ng.xpath("//meta/@content").to_s
-    new_url = new_url.gsub('0;url=','')
+#    vacancies_urls.each do | name, href |
+#      print "#{name} #{href}\n"
+#    end
+    if add_pages
+      pages_urls = {}
+      ng.css('li[data-qa="pager-page"]>a.HH-Pager-Control').each do |alink|
+        # ignore '...' page
+        page = alink.text
+        if page != '...'
+          page_id = page.to_i
+          if page_id > worker_page_id
+            puts page_id.class
+            pages_urls[page_id] = main_server + alink['href']
+          end
+        end
+      end
+      pages_urls.each do | page_id, link |
+        add_pages_flag = page_id == pages_urls.keys.max
+        print "#{keyword} #{link} #{page_id} #{add_pages_flag}\n"
+        DownloadWorker.perform_async(keyword, link, page_id, add_pages = add_pages_flag)
+      end
 
-    response = tg.get new_url, headers
 
-    new_url = response['location']
-    response = tg.get new_url, headers
-    puts response.body
+      print pages_urls
+    end
   end
 end
